6ecf45a60783384ebc830c15fa0140f38adc0320
7AndreyArt7 <7andron7art@gmail.com>
2025-04-13 23:33:18 +0300

    lesson-03-02

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,13 +1,16 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-// function isEven() {
-//     let sum = 0;
-//     while (sum / 2) {
-//         if (sum / 2) {
-//             sum = true;
-//             sum += 1;
-//         } else {
-//             sum = false;
-//         }
-//
+function isEven(number) {
+
+    if ( Math.floor(number / 2) === number / 2 ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+isEven(10)
+
+console.log(isEven());
