151d86ae1e4300484c97b50dc57901da59ca6945
7AndreyArt7 <7andron7art@gmail.com>
2025-04-24 00:04:23 +0300

    lesson-04-03

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -21,23 +21,21 @@ function findCommonElements(array1, array2) {
     let newArray = []
     let newArray0 = []
 
-
     for (let i = 0; i < array1.length; i++) {
         for (let j = 0; j < array2.length; j++) {
-            if (array1[i] === array2[j]) {
+            if (array1[i] === array2[j] && array1.length===array2.length) {
                 if (!includesElement(newArray, array1[i])) {
                     newArray.push(array1[i])
                     if (!includesElement(newArray, array2[j])) {
                         newArray.push(array2[j])
-                        return newArray
-                    }else {
-                        return newArray0
                     }
+                }else {
+                    return newArray0
                 }
             }
         }
     }
-
+    return newArray
 
 }
 
