60442789469eeb0d273788c8e9f0350acb6773ff
7AndreyArt7 <7andron7art@gmail.com>
2025-04-24 00:26:21 +0300

    lesson-04-03

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -22,14 +22,12 @@ function findCommonElements(array1, array2) {
 
     for (let i = 0; i < array1.length; i++) {
         for (let j = 0; j < array2.length; j++) {
-                    if (array1[i] === array2[j] && array1.length === array2.length) {
-                        newArray.push(array2[j])
-                    }
-                }
+            if (array1[i] === array2[j]) {
+                newArray.push(array2[j])
+            }
+        }
     }
     return newArray
-
-
 }
 
 
