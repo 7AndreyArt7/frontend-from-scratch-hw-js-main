e7fc65c0ab0719c9dc7b0d87a59669c7c349a44f
7AndreyArt7 <7andron7art@gmail.com>
2025-05-14 14:35:22 +0300

    lesson-07-02

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -3,12 +3,19 @@
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
-function isNumeric(str) {
+ function isNumeric(str) {
   // your code
+
+     if (str.trim() === ``){
+         return false
+     }
+    return !isNaN(Number(str))
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
